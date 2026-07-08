71e94a70ab513890fbb2d4aef79ae4dceaea53d4
Paras Chaudhary <paraschaudhary.0125@gmail.com>
2026-07-08 22:41:27 +0530

    Time: 3 ms (19.93%), Space: 43.1 MB (11.12%) - LeetHub

--- 0504-base-7/0504-base-7.java
+++ 0504-base-7/0504-base-7.java
@@ -2,13 +2,12 @@ class Solution {
     public String convertToBase7(int num) {
         if(num==0) return "0";
         String s="";
-        int n=num;
+        Boolean n=num<0;
         while(num!=0){
-            int r=num%7;
-            s=String.valueOf(Math.abs(r))+s;
+            s=String.valueOf(Math.abs(num%7))+s;
             num=num/7;
         }
-        if(n<0){
+        if(n){
         s="-"+s;
         }
         return s;
